feb3c942e36a30be831fe1c2664a46ecc2266cd3
Christoffer Ekberg <ce222pc@student.lnu.se>
2016-10-24 15:19:32 +0200

    Implement soft 17 hit rule

---
+++ BlackJack/model/rules/Soft17HitStrategy.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BlackJack.model.rules
+{
+    class Soft17HitStrategy : IHitStrategy
+    {
+        private const int g_hitLimit = 17;
+
+        public bool DoHit(model.Player a_dealer)
+        {
+            if (a_dealer.CalcScore() < g_hitLimit)
+            {
+                return true;
+            }
+
+            if (a_dealer.CalcScore() == 17)
+            {
+                return a_dealer.GetHand().Any(c => c.GetValue() == Card.Value.Ace);
+            }
+
+            return false;
+
+        }
+    }
+}
