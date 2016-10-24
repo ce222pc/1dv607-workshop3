0c47a2d57c4c96189fd4c0e6b0d943bd44876abc
Christoffer Ekberg <ce222pc@student.lnu.se>
2016-10-24 14:33:23 +0200

    Implement Game::Stand() and Dealer::Stand()

--- BlackJack/model/Dealer.cs
+++ BlackJack/model/Dealer.cs
@@ -46,6 +46,21 @@ namespace BlackJack.model
             return false;
         }
 
+        public bool Stand()
+        {
+            if (m_deck != null)
+            {
+                ShowHand();
+                while (m_hitRule.DoHit(this))
+                {
+                    Hit(this);
+                }
+
+                return true;
+            }
+            return false;
+        }
+
         public bool IsDealerWinner(Player a_player)
         {
             if (a_player.CalcScore() > g_maxScore)
