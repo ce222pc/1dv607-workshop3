6068686d1494613928e3a95a24f970096b24d92c
Daniel Sundberg <dnlsndbrg@gmail.com>
2016-10-26 13:19:48 +0200

    more verbose variable names

--- BlackJack/Program.cs
+++ BlackJack/Program.cs
@@ -9,9 +9,9 @@ namespace BlackJack
     {
         static void Main(string[] args)
         {
-            model.Game g = new model.Game();
-            view.IView v = new view.SimpleView(); // new view.SwedishView();
-            controller.PlayGame ctrl = new controller.PlayGame(g, v);
+            model.Game game = new model.Game();
+            view.IView view = new view.SimpleView(); // new view.SwedishView();
+            controller.PlayGame ctrl = new controller.PlayGame(game, view);
 
             while (ctrl.Play());
         }
